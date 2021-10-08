bffd7936527ed4cb0c18df6e5ca49af9ddbdd2fc
ChadT99 <chadt8299@gmail.com>
2021-10-08 09:56:39 -0500

    Finished project

--- app.py
+++ app.py
@@ -13,7 +13,34 @@ saved_entertainment = ''
 def select_destination():
     random_number = random.randint(0,2)
     saved_destination = (destinations_list[random_number])
-    print(saved_destination)
+
+def select_restuarant():
+    random_number = random.randint(0,2)
+    saved_restaurant = (restaurants_list[random_number])
+
+def select_transportation():
+    random_number = random.randint(0,2)
+    saved_transportation = (transportation_list[random_number])
+
+def select_entertainment():
+    random_number = random.randint(0,2)
+    saved_entertainment = (entertainment_list[random_number])
+
+def reroll():
+    print(f'Current trip is: {saved_destination}, {saved_restaurant}, {saved_transportation}, {saved_entertainment}')
+    user_choice = str(input("Do you like these options? (Yes or No): "))
+    if user_choice == 'No':
+         select_destination()
+         select_restuarant()
+         select_transportation()
+         select_entertainment()
+         reroll()
+    print(f'Your trip is: {saved_destination}, {saved_restaurant}, {saved_transportation}, {saved_entertainment}')
+
 
 
 select_destination()
+select_restuarant()
+select_transportation()
+select_entertainment()
+reroll()
